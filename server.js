0571c90fa66335b8ff2191c4386bc6c62e25c1d8
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-07-22 00:43:02 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const Nexmo = require("nexmo");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/sendsms", (req, res) => {
+  const nexmo = new Nexmo({
+    apiKey: 45330349,
+    apiSecret: "pE22nV4Oeg2QBRjk"
+  });
+
+  const from = "6282281817090";
+  const to = req.body.to;
+  const text = req.body.text;
+
+  nexmo.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (responseData.messages[0]["status"] === "0") {
+        res.send({
+          status: "success",
+          data: [],
+          err: null
+        });
+      } else {
+        res.send({
+          status: "error",
+          data: [],
+          err: responseData.messages[0]["error-text"]
+        });
+      }
+    }
+  });
+});
+
+app.post("/sendsms/edo", (req, res) => {
+  const nexmo = new Nexmo({
+    apiKey: 45330349,
+    apiSecret: "pE22nV4Oeg2QBRjk"
+  });
+
+  const from = "6282281817090";
+  const to = req.body.to;
+  const text = req.body.text;
+
+  nexmo.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (responseData.messages[0]["status"] === "0") {
+        res.send({
+          status: "success",
+          data: [],
+          err: null
+        });
+      } else {
+        res.send({
+          status: "error",
+          data: [],
+          err: responseData.messages[0]["error-text"]
+        });
+      }
+    }
+  });
+});
+
+const port = process.env.PORT || 4321;
+app.listen(port, process.env.IP, () => {
+  console.log("server berjalan pada port : ", port);
+});
