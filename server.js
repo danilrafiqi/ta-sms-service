e2fb9652a0a59fceb6d0891488f69038a058777b
Muhamad Danil Rafiqi <danil.rafiqi@gmail.com>
2019-07-22 14:57:46 +0700

    Initial commit

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/sendsms", (req, res) => {
+app.post("/", (req, res) => {
   const nexmo = new Nexmo({
-    apiKey: 45330349,
+    apiKey: "45330349",
     apiSecret: "pE22nV4Oeg2QBRjk"
   });
 
@@ -36,13 +36,13 @@ app.post("/sendsms", (req, res) => {
   });
 });
 
-app.post("/sendsms/edo", (req, res) => {
+app.post("/edo", (req, res) => {
   const nexmo = new Nexmo({
-    apiKey: 45330349,
-    apiSecret: "pE22nV4Oeg2QBRjk"
+    apiKey: "b0040e97",
+    apiSecret: "SS7MO2kKHfUCsm2d"
   });
 
-  const from = "6282281817090";
+  const from = "6282279266979";
   const to = req.body.to;
   const text = req.body.text;
 
